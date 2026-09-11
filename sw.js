b05e650f3362f39e95892606fcaf6914029d73bc
GraffitiWizard <adam@hetrick.me>
2026-09-11 16:44:45 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Offline cache for Frequency & Breath Studio. Bump CACHE name when index.html changes.
+const CACHE = 'breathe-v2';
+const FILES = ['./', './index.html'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+    const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res;
+  }).catch(() => caches.match('./index.html'))));
+});
